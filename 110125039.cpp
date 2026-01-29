51c7f7fe5f08bb9024ae85d38cbedcae5981ed5a
Dong07-a <dongn9064@gmail.com>
2026-01-29 07:56:16 +0700

    Add C program of <110125039_NGUYENCHIDONG>

---
+++ 110125039.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main()
+ {
+ 	int n,m;
+ 	printf("Nhap n:");scanf("%d",&n);
+ 	printf("Nhap m:");scanf("%d",&m);
+ 	printf("\nTong = %d",n+m);
+
+	
+ 	printf("\nHieu = %d",n-m);
+	 
+ 	printf("\nTich = %d",n*m);
+ 	printf("\nThuong = %f",(float)n/m); }
